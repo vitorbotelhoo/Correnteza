8ff0d16eee545507c15f61c2fcbdec3fde30f82a
Vitor Botelho <botelhoavitor@gmail.com>
2024-06-11 01:52:31 -0300

    Atualizando de acordo com projeto local

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,104 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container}> 
       <Head>
         <title>Correnteza Estúdio</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Favicon.png" />
       </Head>
 
+      <header>
+        
+      </header>
       <main>
-        <h1>Transformamos agências e startups em projetos encantadores com entregas encantadoras..</h1>
+        <section className={styles.hero}>
+          <div className={styles.centeredcontainer}>
+            <img className ={styles.logoonhero} src="/Img Logo.png"></img>
+            <h1>Encantamos a sua empresa com a direção certa.</h1>
+          </div>
+        </section>
+        <section className={styles.portfoliosec}>
+          <div className={styles.centeredcontainer}>
+            <h2>Projetos em evidência</h2>
+            <div className={styles.cardsContainer}>
+
+              <div className={styles.portfoliocard_hof}>
+                  <h3>Teste</h3>
+                  <p>Desc</p>
+                  <button>Teste</button>
+              </div>
+
+              <div className={styles.portfoliocard_hof}>
+                  <h3>Teste</h3>
+                  <p>Desc</p>
+                  <button>Teste</button>
+              </div>
+
+              <div className={styles.portfoliocard_hof}>
+                  <h3>Teste</h3>
+                  <p>Desc</p>
+                  <button>Teste</button>
+              </div>
+              
+            </div>
+            <div className={styles.rightalign}>
+              <button className={styles.customcursor_fogo}>Veja mais projetos aqui <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 8.26568L8.58856 8.27491L8.58856 2.4262L1.01476 10L0 8.98524L7.5738 1.41144L1.73432 1.42066V3.44089e-07L10 0V8.26568Z" fill="#B2D9FC"/>
+</svg>
+</button>
+            </div>
+          </div>
+        </section>
       </main>
 
       <style jsx>{`
-        main {
+
+        h1, h2, h3, p {
+          margin: 0px;
+      }
+
+        section {
           padding: 24px;
+        }
+
+        h1 {
+          font-size: 64px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 100%;
+          text-align: center;
+        }
+
+        h2 {
+          font-size: 36px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 110%;
+          text-align: center;
+        }
+
+        button {
+          font-size: 18px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 90%;
+          text-align: center;
+          padding: 8px 12px;
+          border-radius: 12px;
+          font-family: Gabarito;
+          color: #B2D9FC;
+          background-color: #08152C;
+          border: 0px solid;
+          transition: transform .2s;
+          }
+
+        button:hover {
+        transform: scale(1.05);
+        }
+        
+        main {
+          color: #fff;
+          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
@@ -41,27 +126,17 @@ export default function Home() {
           color: inherit;
         }
         code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Gabarito;
         }
       `}</style>
 
       <style jsx global>{`
         html,
         body {
-          background-color: #08152C;
-          color: #B2D9FC;
           padding: 0;
           margin: 0;
           font-family: "Gabarito", sans-serif;
